0d3aab6e90a6cb96eaf6e6e293e736db3f74dde9
Alam Talash <alam1386@gmail.com>
2019-02-28 09:44:14 -0500

    Stylesheet updates

---
+++ themes/inhabitent/page-shop.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * The template for displaying product archive.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+  
+  <div id="primary" class="content-area container">
+    <main id="main" class="site-main" role="main">
+
+    <?php if ( have_posts() ) : ?>
+      <header class="page-header">
+          <h1 class="page-title">Shop Stuff</h1>
+          <ul class="products">
+            <?php
+          $argsarr = array (
+            'orderby' => 'name',
+            'order' => 'ASC',
+            'taxonomy' => 'product_type'
+          ); 
+          $categories = get_terms($argsarr );
+          foreach ( $categories as $category ) {
+            $categorylink = home_url('/') . $category->taxonomy .'/'. $category->slug;
+            ?>
+            <li>
+              <p>
+                <a href="<?php echo $categorylink ?>"> <?php echo $category->name?></a>
+              </p>
+            </li>
+        <?php
+          }
+        ?>
+      </ul>
+      </header><!-- .page-header -->
+
+      <?php 
+      
+      $query_shop = array (
+        'orderby' => 'title',
+        'order' => 'ASC',
+        'category_id'=> 16,
+        'posts_per_page' => 30,
+        'taxonomy' => 'product_type'
+      ); 
+      $new_query = new WP_Query($query_shop);
+      ?>
+      <div class="product-grid">
+      <?php while ( $new_query->have_posts() ) : $new_query->the_post(); ?>
+
+        <div class="product-grid-item">
+          <div class="thumbnail-wrapper">
+            <a href="<?php echo esc_url(the_permalink());?>">
+              <?php the_post_thumbnail( 'large' ); ?>
+            </a>
+          </div>
+          <div class="product-info">
+            <?php the_title('<h2 class="entry-title">', '</h2>' ); ?>
+            <span class="price"><?php echo CFS()->get( 'price' ); ?></span>          
+            </div>
+        </div>
+
+      <?php endwhile; ?>
+    </div>
+    <?php else : ?>
+
+      <?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+    <?php endif; ?>
+
+    </main><!-- #main -->
+  </div><!-- #primary -->
+
+<?php get_footer(); ?>
---
+++ themes/inhabitent/single.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The template for displaying all single posts.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+<div class="find-us-container container">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+
+			<div class="entry-content">
+              
+              <div class="social-buttons">
+                <button type="button" class="black-btn"><i class="fab fa-facebook-square"></i> Like</button>
+                <button type="button" class="black-btn"><i class="fab fa-twitter-square"></i> Tweet</button>
+                <button type="button" class="black-btn"><i class="fab fa-pinterest-square"></i> Pin</button>
+              </div>
+            </div><!-- .entry-content -->
+			<?php
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+			?>
+			
+		<button type="button" id="close-comments">Close Comments </button>
+		<?php endwhile; // End of the loop. ?>
+		
+		</main><!-- #main -->
+		</div><!-- #primary -->
+			<div class="sidebar-widget-area"> 
+				<div class="contact">
+					<h3>CONTACT INFO</h3> 
+					<p><i class="fas fa-envelope"></i> <a href="#">info@inhabitent.com</a></p>
+					<p><i class="fas fa-phone"></i> 778-444-3432</p>
+					<p><i class="fas fa-map-marker-alt"></i> 462 Wellington St W #101, Toronto, ON M5V 1E3</p>
+				</div>
+				<?php get_sidebar(); ?>
+			</div>
+		</div>
+		</div>
+<?php get_footer(); ?>
